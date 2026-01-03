ba0fa4b04cda6ecedf4b9ae665cd4ca64ad787e3
luisgrdz <luis.rodriguez3137@gmail.com>
2026-01-03 17:54:00 -0600

    refactor: Improve formatting and consistency in app.php middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // AÑADE ESTA LÍNEA para confiar en el proxy de Render
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
